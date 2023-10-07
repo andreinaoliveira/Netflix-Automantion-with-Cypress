718296e6194fa8621ac4f9f4f10d97d388a222ce
Andreina Oliveira <andreinaholiveira@gmail.com>
2023-10-06 20:18:09 -0400

    feat: create login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import pageLogin from '../pages/Login'
+Cypress.Commands.add('login', (user) => {
+    cy.session([user], () => {
+        pageLogin.go()
+        pageLogin.fillForm(user)
+        pageLogin.submit()
+    },
+        {
+            cacheAcrossSpecs: true
+        })
+    cy.visit('/browse')
+})
