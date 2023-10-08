c5a2fa332ec33a9ca6bebd8fe7f5edb6e63bd718
Andreina Oliveira <andreinaholiveira@gmail.com>
2023-10-07 20:51:10 -0400

    fix: #2 checking cookier and including validation

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,24 @@
 import pageLogin from '../pages/Login'
+
 Cypress.Commands.add('login', (user) => {
+    cy.step('Checking session')
+    
     cy.session([user], () => {
-        pageLogin.go()
-        pageLogin.fillForm(user)
-        pageLogin.submit()
-    },
-        {
-            cacheAcrossSpecs: true
+        cy.getCookie('NetflixId').then((cookie) => {
+            if (cookie) {
+                return;
+            }
+            pageLogin.go()
+            pageLogin.fillForm(user)
+            pageLogin.submit()
         })
-    cy.visit('/browse')
+    },
+    {
+        validate() {
+            cy.visit('/Browse')
+            cy.url().should('not.equal', 'https://www.netflix.com/login')
+            cy.step('Logged in')
+        },
+        cacheAcrossSpecs: true,
+    })
 })
